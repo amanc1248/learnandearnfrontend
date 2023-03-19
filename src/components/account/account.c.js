e5a704f6e0e8ffc74dc721a0df091d5672eccf7a
amanc1248 <amanc1248@gmail.com>
2023-03-19 22:11:29 +0545

    Add: account popup

---
+++ src/components/account/account.c.js
@@ -0,0 +1,5 @@
+import { AccountP } from "./account.p"
+import "../../styles/account.css"
+export const AccountC = ()=>{
+    return <AccountP className="account__c__div"></AccountP>
+}
---
+++ src/components/account/account.p.js
@@ -0,0 +1,38 @@
+import "../../styles/account.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBook,
+  faGear,
+  faCircleUser,
+} from "@fortawesome/free-solid-svg-icons";
+import {Link} from "react-router-dom";
+export const AccountP = () => {
+  return (
+    <div className="account__p__div">
+      <Link className="account__p__individual" to="/overview/1" style={{textDecoration:"none"}}>
+        <FontAwesomeIcon
+          className="account__login__icon"
+          icon={faBook}
+          style={{ fontSize: "20px"}}
+        />
+        <div>Learnigns</div>
+      </Link>
+      <Link className="account__p__individual" to="/overview/2" style={{textDecoration:"none"}}>
+        <FontAwesomeIcon
+          className="account__login__icon"
+          icon={faCircleUser}
+          style={{ fontSize: "20px" }}
+        />
+        <div>Account</div>
+      </Link>
+      <Link className="account__p__individual" to="/overview/3" style={{textDecoration:"none"}}>
+        <FontAwesomeIcon
+          className="account__login__icon"
+          icon={faGear}
+          style={{ fontSize: "20px" }}
+        />
+        <div>Payment And Plan</div>
+      </Link>
+    </div>
+  );
+};
