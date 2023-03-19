d5f26537d139b77219cd821d293a8b2ab861db01
amanc1248 <amanc1248@gmail.com>
2023-03-19 22:12:02 +0545

    Implemented: checking login in header

--- src/components/header/header.c.js
+++ src/components/header/header.c.js
@@ -1,13 +1,45 @@
+import { useEffect, useState } from "react";
+import { checkLogin } from "../../actions/login.actions";
 import { HeaderP } from "./header.p";
 
 export const HeaderC = ({
   showRegistrationModal,
   showHideRegistrationModal,
 }) => {
+
+// useStates
+const [showAccounts, setShowAccounts] = useState(false);
+const [isLoggedIn, setIsLoggedIn] = useState(false);
+const [userDetails, setUserDetails] = useState(null);
+
+// use effects
+useEffect(() => {
+  const token = localStorage.getItem("userToken");
+  if(token){
+    checkLogin({ token })
+    .then((response) => {
+      setUserDetails(response);
+      setIsLoggedIn(true);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+}, []);
+
+// functions
+const handleShowAccounts = (value)=>{
+  setShowAccounts(value);
+}
+
   return (
     <HeaderP
       showRegistrationModal={showRegistrationModal}
       showHideRegistrationModal={showHideRegistrationModal}
+      handleShowAccounts={handleShowAccounts}
+      showAccounts={showAccounts}
+      isLoggedIn={isLoggedIn}
+      userDetails={userDetails}
     ></HeaderP>
   );
 };
--- src/components/header/header.p.js
+++ src/components/header/header.p.js
@@ -1,21 +1,60 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import "../../styles/header.css"
- export const  HeaderP= ({showHideRegistrationModal}) =>{
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
+import "../../styles/header.css";
+import "../../styles/account.css";
+import { AccountC } from "../account/account.c";
+export const HeaderP = ({
+  showHideRegistrationModal,
+  showAccounts,
+  handleShowAccounts,
+  isLoggedIn,
+  userDetails,
+}) => {
   return (
     <Navbar collapseOnSelect expand="lg" bg="white" variant="light">
       <Container fluid className="p-0 header__container__div">
-        <Navbar.Brand href="#home" className='brand__name'>CODE VALLEY</Navbar.Brand>
+        <Navbar.Brand href="#home" className="brand__name">
+          CODE VALLEY
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-          </Nav>
-          <Nav className='ml-auto'>
-            <button className='enroll__for__free__button' onClick={()=>{showHideRegistrationModal(true)}}>Enroll For Free</button>
+          <Nav className="me-auto"></Nav>
+          <Nav className="ml-auto nav__div">
+            {isLoggedIn && (
+              <div className="account__login">
+                <div className="username__welcome__text">
+                  <div className="username__div">{userDetails?.name}</div>
+                  <div className="welcome__back__text">Welcome !</div>
+                </div>
+                <FontAwesomeIcon
+                  className="account__login__icon"
+                  icon={faCircleUser}
+                  style={{ fontSize: "40px" }}
+                  onClick={() => {
+                    handleShowAccounts(!showAccounts);
+                  }}
+                />
+                {showAccounts && (
+                  <div className="account__div__popup">
+                    <AccountC></AccountC>
+                  </div>
+                )}
+              </div>
+            )}
+            {!isLoggedIn && <button
+              className="enroll__for__free__button"
+              onClick={() => {
+                showHideRegistrationModal(true);
+              }}
+            >
+              Enroll For Free
+            </button>}
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
-}
+};
