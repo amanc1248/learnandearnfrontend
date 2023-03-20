7d99824cdeadc127b10d72c1f00398b4aec4e42b
amanc1248 <amanc1248@gmail.com>
2023-03-20 20:25:49 +0545

    update: overviewScreen in route

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { HomeScreen } from "./screens/HomeScreen";
 import { CourseContentScreen } from "./screens/CourseContentScreen/index";
-import { OverviewScreepC } from "./screens/OverviewScreen/OverviewScreen.c";
+import { OverViewScreen } from "./screens/OverviewScreen/OverviewScreen";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomeScreen />}></Route>
-        <Route path="/overview/:id?" element={<OverviewScreepC />}></Route>
+        <Route path="/overview/:id?" element={<OverViewScreen />}></Route>
         <Route
           path="/fullstack-web-development"
           element={<CourseContentScreen />}
