bca8bbd20aee9845963d4a33f77a70884f4535ff
amanc1248 <amanc1248@gmail.com>
2023-03-22 20:01:38 +0545

    Add: changeEmail variable to control

--- src/screens/OverviewScreen/Account/AccountEmail/AccountEmail.c.js
+++ src/screens/OverviewScreen/Account/AccountEmail/AccountEmail.c.js
@@ -1,11 +1,17 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../../../../HOCs/AuthenticateLogin/AuthenticateLogin.hoc.c";
-import { AccountEmailP } from "./AccountEmail.p"
+import { AccountEmailP } from "./AccountEmail.p";
 
-export const AccountEmailC = ()=>{
-
-    // use context
+export const AccountEmailC = () => {
+  // useState
+  const [changeEmail, setChangeEmail] = useState(false);
+  // use context
   const userDetails = useContext(UserContext);
 
-    return <AccountEmailP email={userDetails?.email}></AccountEmailP>
-}
+  return (
+    <AccountEmailP
+      email={userDetails?.email}
+      changeEmail={changeEmail}
+    ></AccountEmailP>
+  );
+};
--- src/screens/OverviewScreen/Account/AccountEmail/AccountEmail.p.js
+++ src/screens/OverviewScreen/Account/AccountEmail/AccountEmail.p.js
@@ -1,8 +1,11 @@
-import "../../../../styles/overviewScreen.css"
-export const AccountEmailP = ({email}) => {
+import "../../../../styles/overviewScreen.css";
+export const AccountEmailP = ({ email, changeEmail }) => {
   return (
     <div>
-      <div className="account__subtitle">Email</div>
+      <div className="subtitle__plus__change__button">
+        <div className="account__subtitle">Email</div>
+        <div className="email__password__change__button">Change</div>
+      </div>
       <input type="email" value={email} disabled={true} />
     </div>
   );
