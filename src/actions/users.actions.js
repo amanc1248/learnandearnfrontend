f2d3c453f0bd67cd93aaa88f8f546c10591c49cf
amanc1248 <amanc1248@gmail.com>
2023-03-26 21:57:33 +0545

    Add: changeuserPassword action

--- src/actions/users.actions.js
+++ src/actions/users.actions.js
@@ -1,42 +1,81 @@
-import { CHANGE_USER_EMAIL_ROUTE } from "../constants/routes/userRoutes.constants";
+import {
+  CHANGE_USER_EMAIL_ROUTE,
+  CHANGE_USER_PASSWORD_ROUTE,
+} from "../constants/routes/userRoutes.constants";
 import { createAxiosInstance } from "../utils/axiosInstance.utils";
 const axiosInstance = createAxiosInstance();
 
 // reset user password
 export const resetUserPassword = ({ email, password }) => {
-    return new Promise((resolve, reject) => {
-      const config = {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      };
-      axiosInstance
-        .put("/user/resetPassword", { email, password }, config)
-        .then((response) => {
-          resolve(response);
-        })
-        .catch((error) => {
-          reject(error)
-        });
-    });
-  };
-
-  // change email
-  export const changeUserEmail = ({id, email, newEmail, token})=>{
-    return new Promise((resolve, reject)=>{
-      const config ={
-        headers:{
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${token}`,
-        }
-      };
-      const data ={
-        id, email, newEmail
-      }
-      axiosInstance.put(CHANGE_USER_EMAIL_ROUTE,data, config).then((response)=>{
-        resolve(response.data);
-      }).catch((error)=>{
-        reject(error);
+  return new Promise((resolve, reject) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    axiosInstance
+      .put("/user/resetPassword", { email, password }, config)
+      .then((response) => {
+        resolve(response);
       })
-    })
-  }
+      .catch((error) => {
+        reject(error);
+      });
+  });
+};
+
+// change email
+export const changeUserEmail = ({ id, email, newEmail, token }) => {
+  return new Promise((resolve, reject) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const data = {
+      id,
+      email,
+      newEmail,
+    };
+    axiosInstance
+      .put(CHANGE_USER_EMAIL_ROUTE, data, config)
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+};
+
+// change user password
+export const changeUserPassword = ({
+  email,
+  token,
+  newPassword,
+  currentPassword,
+}) => {
+  return new Promise((resolve, reject) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const data = {
+      email,
+      newPassword,
+      currentPassword,
+    };
+    axiosInstance
+      .put(CHANGE_USER_PASSWORD_ROUTE, data, config)
+      .then((response) => {
+        resolve(response);
+      })
+      .catch((error) => {
+        console.log(error.data);
+        reject(error.response.data);
+      });
+  });
+};
