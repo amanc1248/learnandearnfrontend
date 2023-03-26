b9f038d67f214f9080df82af9b017eaf79bffde5
amanc1248 <amanc1248@gmail.com>
2023-03-26 13:18:47 +0545

    Implement: changeEmail functionality

--- src/screens/OverviewScreen/Account/AccountEmail/AccountEmail.p.js
+++ src/screens/OverviewScreen/Account/AccountEmail/AccountEmail.p.js
@@ -1,12 +1,24 @@
 import "../../../../styles/overviewScreen.css";
-export const AccountEmailP = ({ email, changeEmail }) => {
+import { ChangeEmailC } from "./ChangeEmail.c";
+export const AccountEmailP = ({ email, changeEmail, handleOnChange }) => {
   return (
     <div>
-      <div className="subtitle__plus__change__button">
-        <div className="account__subtitle">Email</div>
-        <div className="email__password__change__button">Change</div>
-      </div>
-      <input type="email" value={email} disabled={true} />
+      {changeEmail ? (
+        <ChangeEmailC handleOnChange={handleOnChange}></ChangeEmailC>
+      ) : (
+        <div>
+          <div className="subtitle__plus__change__button">
+            <div className="account__subtitle">Email</div>
+            <div
+              className="email__password__change__button"
+              onClick={()=>{handleOnChange(true)}}
+            >
+              Change
+            </div>
+          </div>
+          <input type="email" value={email} disabled={true} />
+        </div>
+      )}
     </div>
   );
 };
