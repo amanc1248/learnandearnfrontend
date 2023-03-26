8edb615a3363f0753d7e675c66cc52099004b0de
amanc1248 <amanc1248@gmail.com>
2023-03-26 21:58:16 +0545

    Add: change password functionality

--- src/screens/OverviewScreen/Account/AccountPassword/ChangePasswordStep1.c.js
+++ src/screens/OverviewScreen/Account/AccountPassword/ChangePasswordStep1.c.js
@@ -1,4 +1,10 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { changeUserPassword } from "../../../../actions/users.actions";
+import { USER_TOKEN_CONSTANT } from "../../../../constants/localstorage.constants";
+import { UserContext } from "../../../../HOCs/AuthenticateLogin/AuthenticateLogin.hoc.c";
+import { getFromLocalStorage } from "../../../../utils/localStorage.utils";
+import { validatePassword } from "../../../../utils/password.utils";
+import { ChangePasswordContext } from "./AccountPassword.c";
 import { ChangePasswordStep1P } from "./ChangePasswordStep1.p";
 
 export const ChangePasswordStep1C = () => {
@@ -6,6 +12,16 @@ export const ChangePasswordStep1C = () => {
   const [newPassword, setNewPassword] = useState("");
   const [confirmNewPassword, setConfirmNewPassword] = useState("");
   const [currentPassword, setCurrentPassword] = useState("");
+  const [changePasswordStatus, setChangePasswordStatus] = useState({
+    error: false,
+    text:""
+  })
+
+      // use context
+      const userContextData = useContext(UserContext);
+      const {userDetails} = userContextData
+      const {email} = userDetails;
+      const {handleIsShowPasswordChange} = useContext(ChangePasswordContext)
 
   // functions
   const onChangeNewPassword = (value) => {
@@ -17,8 +33,26 @@ export const ChangePasswordStep1C = () => {
   const onChangeCurrentPassword = (value) => {
     setCurrentPassword(value);
   };
-  const handleOnSubmit = ()=>{
-    
+  const handleOnSubmit = (e)=>{
+    e.preventDefault();
+    if(newPassword===confirmNewPassword){
+      const isValidPasswordResult = validatePassword(newPassword);
+      if(isValidPasswordResult===true){
+        const token = getFromLocalStorage(USER_TOKEN_CONSTANT);
+        console.log({email, token, newPassword, currentPassword});
+        changeUserPassword({email, token, newPassword, currentPassword}).then((response)=>{
+          setChangePasswordStatus({error: false, text: "Password Changed Successfully"});
+          handleIsShowPasswordChange(false)
+        }).catch((error)=>{
+          console.log(error);
+          setChangePasswordStatus({error: true, text: error});
+        })
+      }else{
+        setChangePasswordStatus({error: true, text:isValidPasswordResult})
+      }
+    }else{
+      setChangePasswordStatus({error: true, text:"Password did not match"})
+    }
   }
   return (
     <ChangePasswordStep1P
@@ -29,6 +63,7 @@ export const ChangePasswordStep1C = () => {
       onChangeConfirmNewPassword={onChangeConfirmNewPassword}
       onChangeCurrentPassword={onChangeCurrentPassword}
       handleOnSubmit={handleOnSubmit}
+      changePasswordStatus={changePasswordStatus}
     ></ChangePasswordStep1P>
   );
 };
--- src/screens/OverviewScreen/Account/AccountPassword/ChangePasswordStep1.p.js
+++ src/screens/OverviewScreen/Account/AccountPassword/ChangePasswordStep1.p.js
@@ -1,3 +1,4 @@
+import {StatusTextComponentC} from "../../../../components/statusText/StatusTextComponent.c"
 export const ChangePasswordStep1P = ({
   newPassword,
   confirmNewPassword,
@@ -6,13 +7,27 @@ export const ChangePasswordStep1P = ({
   onChangeConfirmNewPassword,
   onChangeCurrentPassword,
   handleOnSubmit,
+  changePasswordStatus,
 }) => {
   return (
     <div>
       <form action="" onSubmit={handleOnSubmit}>
+        {changePasswordStatus.text && <div>
+          <StatusTextComponentC
+          isError={changePasswordStatus.error}
+          statusText={changePasswordStatus.text}
+          ></StatusTextComponentC>
+          </div>}
+        <div className="password__creating__instruction">
+          Make sure your password is at least 8 characters and contains at least
+          1 uppercase letter, 1 lowercase letter, 1 number, and 1 special
+          character (like @#%^). Avoid common passwords or strings like
+          “password”, “qwerty”, or “12345”.
+        </div>
         <div>
           <label htmlFor="newPassword">New Password *</label>
         </div>
+
         <input
           type="password"
           id="newPassword"
@@ -60,7 +75,7 @@ export const ChangePasswordStep1P = ({
             // disabled={disableButton}
             className={`changeEmail__button`}
           />
-          </div>
+        </div>
       </form>
     </div>
   );
