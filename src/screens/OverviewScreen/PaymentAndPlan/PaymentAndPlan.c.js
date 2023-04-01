4715965650673e5c590f00e789ef9b0e9fa603d4
amanc1248 <amanc1248@gmail.com>
2023-04-01 18:45:35 +0545

    Add: boilerplate payment and plan

--- src/screens/OverviewScreen/PaymentAndPlan/PaymentAndPlan.c.js
+++ src/screens/OverviewScreen/PaymentAndPlan/PaymentAndPlan.c.js
@@ -1,5 +1,25 @@
-import { PaymentAndPlanP } from "./PaymentAndPlan.p"
+import { useState } from "react";
+import { PaymentAndPlanP } from "./PaymentAndPlan.p";
 
-export const PaymentAndPlanC = ()=>{
-    return <PaymentAndPlanP></PaymentAndPlanP>
-}
+export const PaymentAndPlanC = () => {
+  // data
+  const tabsList = [
+    { name: "Purchase Details", id: 0 },
+    { name: "Billing History", id: 1 },
+  ];
+
+  // use states
+  const [activeTab, setActiveTab] = useState(0);
+
+  // functions
+  const handleChangeTab = (value) => {
+    setActiveTab(value);
+  };
+  return (
+    <PaymentAndPlanP
+      tabsList={tabsList}
+      handleChangeTab={handleChangeTab}
+      activeTab={activeTab}
+    ></PaymentAndPlanP>
+  );
+};
--- src/screens/OverviewScreen/PaymentAndPlan/PaymentAndPlan.p.js
+++ src/screens/OverviewScreen/PaymentAndPlan/PaymentAndPlan.p.js
@@ -1,7 +1,23 @@
-export const PaymentAndPlanP = () => {
+import "../../../styles/paymentAndPlan.css";
+import { BillingHistoryC } from "./BillingHistory/BillingHistory.c";
+import { PurchaseDetailsC } from "./PurchaseDetails/PurchaseDetails.c";
+export const PaymentAndPlanP = ({ tabsList, handleChangeTab, activeTab }) => {
   return (
     <div>
       <div className="overviewScreenList__tab__title">Payment And Plan</div>
+      <div className="paymentDetails__and__billingHistory__tabs">
+        {tabsList.map((tab) => {
+          return (
+            <div key={tab.id} className={`pDbHT_single ${tab.id===activeTab && "selected__pDbHT_single"}`} onClick={()=>{handleChangeTab(tab.id)}}>
+              {tab.name}
+            </div>
+          );
+        })}
+      </div>
+      <div className="tab__content">
+        {activeTab===0 && <PurchaseDetailsC></PurchaseDetailsC>}
+        {activeTab===1 && <BillingHistoryC></BillingHistoryC>}
+      </div>
     </div>
   );
 };
