a75e35bac1242260f86a3b9160d74a210756a3eb
Aman Chaudhary <amanc1248@gmail.com>
2023-05-03 22:27:14 +0545

    Add: user moddal

--- src/screens/Admin/AdminDashboard/users/UserModal.p.js
+++ src/screens/Admin/AdminDashboard/users/UserModal.p.js
@@ -1,5 +1,6 @@
 import { Modal } from "react-bootstrap";
 import "../../../../styles/userModal.css";
+import { convertUTCToMonthDateYearFormat } from "../../../../utils/date.utils";
 export const UserModalP = ({
   user,
   userDetails,
@@ -7,6 +8,7 @@ export const UserModalP = ({
   changeShowModal,
 }) => {
   console.log("user: ", user);
+
   return (
     <Modal
       show={showModal}
@@ -74,47 +76,92 @@ export const UserModalP = ({
             </table>
           </div>
 
-          {/* subscription details div */}
-          <div className="user__subscription__details__div">
-            <h5>Subscription Details</h5>
+          <div className="row">
+            {/* subscription details div */}
+            <div className="user__subscription__details__div col-lg-6 col-md-6 col-12">
+              <h5>Subscription Details</h5>
 
-            {userDetails?.subscriptions.length ?
-              userDetails?.subscriptions.map((subscription) => (
-                <ol class="list-group list-group-numbered">
-                  <li class="list-group-item d-flex justify-content-between align-items-start">
-                    <div class="ms-2 me-auto">
-                      <div class="fw-bold">
-                        {subscription?.subscriptionType}
-                      </div>
-                      {subscription?.subscriptionStartDate}
-                    </div>
-                    <span class="badge bg-primary rounded-pill">
-                      Rs. {subscription?.subscriptionAmount}
-                    </span>
-                  </li>
-                </ol>
-              )): "No subscription, which is not possible"}
-          </div>
+              <ol class="list-group list-group-numbered">
+                {userDetails?.subscriptions.length
+                  ? userDetails?.subscriptions.map((subscription) => (
+                      <li class="list-group-item d-flex justify-content-between align-items-start">
+                        <div class="ms-2 me-auto">
+                          <div class="fw-bold">
+                            {subscription?.subscriptionType}
+                          </div>
+                          <div className="subscription__start__date">
+                            Start Date:{" "}
+                            {convertUTCToMonthDateYearFormat({
+                              utcDateString:
+                                subscription?.subscriptionStartDate,
+                            })}
+                          </div>
+                          <div className="subscription__end__date">
+                            End Date:{" "}
+                            {convertUTCToMonthDateYearFormat({
+                              utcDateString: subscription?.subscriptionEndDate,
+                            })}
+                          </div>
+                        </div>
+                        <span class="badge bg-primary rounded-pill">
+                          Rs. {subscription?.subscriptionAmount}
+                        </span>
+                      </li>
+                    ))
+                  : "No subscription, which is not possible"}
+              </ol>
+            </div>
+            {/* payment details div */}
+            <div className="user__payment__details__div col-lg-6 col-md-6 col-12">
+              <h5>Payment Details</h5>
+              <ol class="list-group list-group-numbered">
+                {userDetails?.payments.length
+                  ? userDetails?.payments.map((payment) => (
+                      <li class="list-group-item d-flex justify-content-between align-items-start">
+                        <div class="ms-2 me-auto">
+                          <div class="fw-bold">{payment?.method}</div>
+                          <div className="payment__date">
+                            Payment Date:{" "}
+                            {convertUTCToMonthDateYearFormat({
+                              utcDateString: payment?.paymentDate,
+                            })}
+                          </div>
 
-          {/* payment details div */}
-          <div className="user__payment__details__div">
-            <h5>Payment Details</h5>
-            {userDetails?.payments.length ?
-              userDetails?.payments.map((payment) => (
-                <ol class="list-group list-group-numbered">
-                  <li class="list-group-item d-flex justify-content-between align-items-start">
-                    <div class="ms-2 me-auto">
-                      <div class="fw-bold">
-                        {payment?.subscriptionType}
-                      </div>
-                      {payment?.subscriptionStartDate}
-                    </div>
-                    <span class="badge bg-primary rounded-pill">
-                      Rs. {payment?.subscriptionAmount}
-                    </span>
-                  </li>
-                </ol>
-              )): "No payments"}
+                          {payment?.method === "bankTransfer" && (
+                            <div>
+                              <div className="bank__name">
+                                Bank Name: {payment?.bankName}
+                              </div>
+                              <div className="bank__account__number">
+                                Account Number: {payment?.bankAccountNumber}
+                              </div>
+                            </div>
+                          )}
+                          {payment?.method === "walletTransfer" && <div></div>}
+
+                          <div className="payment__image">
+                            <img
+                              src={payment?.paymentImage}
+                              alt="payment"
+                              srcset=""
+                              height={200}
+                            />
+                          </div>
+                        </div>
+                        <div>
+                          <div class="badge bg-primary rounded-pill">
+                            Rs. {payment?.amount}
+                          </div>
+                          <br />
+                          <div className="badge bg-badge text-bg-info rounded-pill">
+                            {payment?.reviewStatus}
+                          </div>
+                        </div>
+                      </li>
+                    ))
+                  : "No payments"}
+              </ol>
+            </div>
           </div>
         </div>
       </Modal.Body>
