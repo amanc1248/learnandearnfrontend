7e0a23ce1cc2e67e87c3f8d94ec0a88dbcd60a15
amanc1248 <amanc1248@gmail.com>
2023-08-28 20:58:17 +0545

    Add: redirect to overview screen after successful login

--- src/components/registrationLoginForm/registrationForm/step3/step3.c.js
+++ src/components/registrationLoginForm/registrationForm/step3/step3.c.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { createUser } from "../../../../actions/registration.actions";
 import { validatePassword } from '../../../../utils/password.utils';
 import { Step3P } from "./step3.p";
+import { useNavigate } from 'react-router-dom';
 
 export const Step3C = ({
   showHideRegistrationModal,
@@ -12,19 +13,22 @@ export const Step3C = ({
   // data
   let confirmPassword;
 
+  // navigate
+  const navigate = useNavigate()
 // use states
 const [password, setPassword] = useState();
 const [registering, setRegistering] = useState(null); 
+const [successRegistration, setSuccessRegistration] = useState(false)
 
 // functions
-  const onHandleSubmit = (e) => {
+  const onHandleSubmit = async(e) => {
     setRegistering(true);
     e.preventDefault();
     if(password===confirmPassword){
       const validatePasswordResult = validatePassword(password);
       if(validatePasswordResult===true){
         const {fullName: name, email} = registrationFormDetails;
-        createUser({name, email, password}).then((response)=>{
+        await createUser({name, email, password}).then((response)=>{
           setRegistering(false);
           setPassword('');
           confirmPassword='';
@@ -33,13 +37,18 @@ const [registering, setRegistering] = useState(null);
             error: true,
             text: "Account Registered Successfully.",
           });
+          setSuccessRegistration(true)
+          navigate("/overview")
+
         }).catch((error)=>{
           setRegistering(false);
+          setSuccessRegistration(true)
           changeRegistrationStatus({
             error: true,
             text: error.response.data,
           });
         })
+
       }else{
         setRegistering(false);
         changeRegistrationStatus({
