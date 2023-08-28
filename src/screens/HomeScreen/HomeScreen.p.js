ba12881728df7eef5298fc4fcd5a59b2feda8824
amanc1248 <amanc1248@gmail.com>
2023-08-28 22:48:28 +0545

    Add: image

--- src/screens/HomeScreen/HomeScreen.p.js
+++ src/screens/HomeScreen/HomeScreen.p.js
@@ -16,26 +16,40 @@ export const HomeScreenP = ({
         <div className="homeScreen__courseOverview__list">
           <ul>
             <li>
-            Coding classes which prepares for the demands of the industry by learning the latest technologies and adhering to industry best practices.
+              Coding classes which prepares for the demands of the industry by
+              learning the latest technologies and adhering to industry best
+              practices.
             </li>
-            <li>Get hands-on experience by working on real-world projects that simulate industry challenges. </li>
             <li>
-            Our course equips students with an extensive skillset covering both front-end and back-end technologies. From crafting engaging user interfaces using HTML, CSS, and JavaScript, to building robust server-side applications with databases and frameworks, students will master the entire spectrum of tools needed to thrive as a professional full-stack web developer.
+              Get hands-on experience by working on real-world projects that
+              simulate industry challenges.{" "}
+            </li>
+            <li>
+              Our course equips students with an extensive skillset covering
+              both front-end and back-end technologies. From crafting engaging
+              user interfaces using HTML, CSS, and JavaScript, to building
+              robust server-side applications with databases and frameworks,
+              students will master the entire spectrum of tools needed to thrive
+              as a professional full-stack web developer.
             </li>
           </ul>
         </div>
-        <div className="enroll__for__free__button button__width" onClick={()=>{showHideRegistrationModal(true)}}>
+        <div
+          className="enroll__for__free__button button__width"
+          onClick={() => {
+            showHideRegistrationModal(true);
+          }}
+        >
           Start Your Coding Journey
         </div>
       </div>
       <div className="col-lg-6 col-12">
         <img
           className="coding__kid__image"
-          src="https://www.strivemath.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fhero-image.caf37ccf.png&w=1920&q=75"
+          src="https://res.cloudinary.com/proudposhak-com/image/upload/v1693242126/banner_p8t0lu.png"
           alt="coding__kid"
         />
       </div>
-
     </div>
   );
 };
