d959a1a77e9cb753e8f29d8e673829cc99ff916a
amanc1248 <amanc1248@gmail.com>
2023-08-30 20:41:36 +0545

    Add: navigating to overiew page

--- src/components/registrationLoginForm/registrationForm/step3/step3.c.js
+++ src/components/registrationLoginForm/registrationForm/step3/step3.c.js
@@ -1,62 +1,75 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { createUser } from "../../../../actions/registration.actions";
-import { validatePassword } from '../../../../utils/password.utils';
+import { validatePassword } from "../../../../utils/password.utils";
 import { Step3P } from "./step3.p";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import { login } from "../../../../actions/login.actions";
+import { saveToLocalStorage } from "../../../../utils/localStorage.utils";
+import { USER_TOKEN_CONSTANT } from "../../../../constants/localstorage.constants";
 
 export const Step3C = ({
   showHideRegistrationModal,
   registrationFormDetails,
   changeRegistrationStatus,
 }) => {
-
   // data
   let confirmPassword;
 
   // navigate
-  const navigate = useNavigate()
-// use states
-const [password, setPassword] = useState();
-const [registering, setRegistering] = useState(null); 
-const [successRegistration, setSuccessRegistration] = useState(false)
+  const navigate = useNavigate();
+  // use states
+  const [password, setPassword] = useState();
+  const [registering, setRegistering] = useState(null);
+  const [successRegistration, setSuccessRegistration] = useState(false);
 
-// functions
-  const onHandleSubmit = async(e) => {
+  // functions
+  const onHandleSubmit = async (e) => {
     setRegistering(true);
     e.preventDefault();
-    if(password===confirmPassword){
+    if (password === confirmPassword) {
       const validatePasswordResult = validatePassword(password);
-      if(validatePasswordResult===true){
-        const {fullName: name, email} = registrationFormDetails;
-        await createUser({name, email, password}).then((response)=>{
-          setRegistering(false);
-          setPassword('');
-          confirmPassword='';
-          showHideRegistrationModal(false);
-          changeRegistrationStatus({
-            error: true,
-            text: "Account Registered Successfully.",
+      if (validatePasswordResult === true) {
+        const { fullName: name, email } = registrationFormDetails;
+        await createUser({ name, email, password })
+          .then((response) => {
+            // setRegistering(false);
+            // setPassword('');
+            // confirmPassword='';
+            // showHideRegistrationModal(false);
+            // changeRegistrationStatus({
+            //   error: true,
+            //   text: "Account Registered Successfully.",
+            // });
+            // setSuccessRegistration(true)
+            // navigate("/overview")
+            login({ email, password })
+              .then((loginResponse) => {
+                saveToLocalStorage({
+                  key: USER_TOKEN_CONSTANT,
+                  value: loginResponse.data,
+                });
+                navigate("/overview");
+              })
+              .catch((loginError) => {
+                console.error(loginError);
+              });
+          })
+          .catch((error) => {
+            setRegistering(false);
+            setSuccessRegistration(true);
+            changeRegistrationStatus({
+              error: true,
+              text: error?.response?.data,
+            });
           });
-          setSuccessRegistration(true)
-          navigate("/overview")
-
-        }).catch((error)=>{
-          setRegistering(false);
-          setSuccessRegistration(true)
-          changeRegistrationStatus({
-            error: true,
-            text: error.response.data,
-          });
-        })
-
-      }else{
+      } else {
         setRegistering(false);
         changeRegistrationStatus({
           error: true,
           text: validatePasswordResult,
         });
       }
-    }else{
+    } else {
       setRegistering(false);
       changeRegistrationStatus({
         error: true,
@@ -70,7 +83,7 @@ const [successRegistration, setSuccessRegistration] = useState(false)
   };
 
   const handleConfirmPasswordChange = (value) => {
-    confirmPassword = value
+    confirmPassword = value;
   };
 
   return (
@@ -79,7 +92,7 @@ const [successRegistration, setSuccessRegistration] = useState(false)
       password={password}
       handlePasswordChange={handlePasswordChange}
       handleConfirmPasswordChange={handleConfirmPasswordChange}
-      registering = {registering}
+      registering={registering}
     ></Step3P>
   );
 };
